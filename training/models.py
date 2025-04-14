ccaaefc2684ee4f5746222093e3ee45bb7be9cf6
Naksha <nakshatra.mitmpl2022@learner.manipal.edu>
2025-04-14 05:58:19 +0000

    Made modifications to burns scenario to prevent never ending loop and add the continue button. Modified achievement backend and models.py

--- training/models.py
+++ training/models.py
@@ -1,26 +1,48 @@
-# Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import timedelta
 
 class Module(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)  # e.g., 'allergy_quiz'
-    max_score = models.PositiveIntegerField(default=50)  # Default based on your quiz
-    passing_score = models.PositiveIntegerField(default=60)  # Minimum to be considered complete
+    slug = models.SlugField(unique=True)
+    max_score = models.PositiveIntegerField(default=50)
+    passing_score = models.PositiveIntegerField(default=60)
     
     def __str__(self):
         return self.title
+
 
 class UserModuleProgress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='module_progress')
     module = models.ForeignKey(Module, on_delete=models.CASCADE)
     score = models.PositiveIntegerField(default=0)
-    completed = models.BooleanField(default=0)
+    completed = models.BooleanField(default=False)
     attempts = models.PositiveIntegerField(default=0)
     last_attempt = models.DateTimeField(auto_now=True)
-    
+    time_spent = models.DurationField(default=timedelta()) 
+    #time_spent = models.IntegerField(default=0)  # Needed for "Quick Thinker" achievement
+
     class Meta:
         unique_together = ('user', 'module')
-    
+
     def __str__(self):
         return f"{self.user.username} - {self.module.title} ({self.score})"
+
+
+class Achievement(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    icon = models.CharField(max_length=100, help_text="CSS class or image name for the achievement icon")
+
+    def __str__(self):
+        return self.title
+
+
+class UserAchievement(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='achievements')
+    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ('user', 'achievement')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.achievement.title}"
