28016c34017c7e04b5ad349d808bc2665106e411
Naksha <nakshatra.mitmpl2022@learner.manipal.edu>
2025-04-14 18:09:44 +0000

    Completed Backend of scenario version 1

--- training/admin.py
+++ training/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Module,UserModuleProgress
+from .models import Module,UserModuleProgress, Scenario, UserScenarioProgress
 
 # Register your models here.
 admin.site.register(Module)
 admin.site.register(UserModuleProgress)
+admin.site.register(Scenario)
+admin.site.register(UserScenarioProgress)
